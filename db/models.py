c6f635773c37b57913060ae5f81edc1bf5226c58
Anatolii Hrechaniuk <genezisium@gmail.com>
2025-12-23 11:56:56 +0200

    Solution2

--- db/models.py
+++ db/models.py
@@ -9,7 +9,8 @@ class Race(models.Model):
 class Skill(models.Model):
     name = models.CharField(max_length=255, unique=True)
     bonus = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE,
+                             related_name="skills")
 
 
 class Guild(models.Model):
@@ -22,5 +23,6 @@ class Player(models.Model):
     email = models.EmailField(max_length=255, unique=False)
     bio = models.CharField(max_length=255)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True,
+                              related_name="players")
     created_at = models.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -10,30 +10,30 @@ def main() -> None:
         players = json.load(read_file)
     for name, info in players.items():
         race_obj, _ = Race.objects.get_or_create(
-            name=info["race"]["name"],
-            description=info["race"]["description"]
-
+            name=info.get("race").get("name"),
+            description=info.get("race").get("description")
         )
-        if info["guild"] is None:
+
+        if info.get("guild") is None:
             guild_obj = None
         else:
             guild_obj, _ = Guild.objects.get_or_create(
-                name=info["guild"]["name"],
-                description=info["guild"]["description"]
+                name=info.get("guild").get("name"),
+                description=info.get("guild").get("description")
             )
 
         player_obj, _ = Player.objects.get_or_create(
             nickname=name,
-            email=info["email"],
-            bio=info["bio"],
+            email=info.get("email"),
+            bio=info.get("bio"),
             race=race_obj,
             guild=guild_obj
         )
 
-        for skill in info["race"]["skills"]:
+        for skill in info.get("race").get("skills"):
             skill_obj, _ = Skill.objects.get_or_create(
-                name=skill["name"],
-                bonus=skill["bonus"],
+                name=skill.get("name"),
+                bonus=skill.get("bonus"),
                 race=race_obj
             )
 
